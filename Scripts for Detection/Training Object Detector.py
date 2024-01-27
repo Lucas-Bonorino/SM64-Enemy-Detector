d255920014993c283919498c8aa5bd2cbac3c0cc
Lucas <72527278+Lucas-Bonorino@users.noreply.github.com>
2024-01-27 19:20:33 -0300

    Alterado o formato do csv e refatorada a função de loading dos dados

--- Scripts for Detection/Training Object Detector.py
+++ Scripts for Detection/Training Object Detector.py
@@ -3,9 +3,8 @@ import tensorflow as tf
 import keras
 import keras_cv
 import numpy as np
-from keras_cv import bounding_box
+from keras_cv import bounding_box, visualization
 import os
-from keras_cv import visualization
 import tqdm
 import sys
 from math import floor
@@ -35,7 +34,6 @@ def Get_BBOX(row):
 def visualize_dataset(inputs, value_range, rows, cols, bounding_box_format):    
     inputs = next(iter(inputs.take(1)))
     images, bounding_boxes = inputs["images"], inputs["bounding_boxes"]
-
     
     visualization.plot_bounding_box_gallery(
         images,
@@ -57,17 +55,17 @@ def Progres_bar(i, num):
     Remaining_Bar=30-Bar_Progress
     print('['+Bar_Progress*'='+' '*Remaining_Bar+']'+str(100*progress)+'% concluido',end='', flush=True)
 
-def Unpack_Raw_Format(Data, bb_format):
-    image = Data['images']
-
-    boxes = keras_cv.bounding_box.convert_format(Data['bounding_boxes']['boxes'], images=image, source='xyxy', target=bb_format)
-
+def Unpack_Raw_Format(Images, Boxes, bb_format):
+    image = Images
+  
+    boxes = keras_cv.bounding_box.convert_format(Boxes['boxes'], images=image, source='xywh', target=bb_format)
+    print(Boxes['classes'])
     bounding_boxes = {
-        "classes": Data['bounding_boxes']['classes'],
+        "classes": Boxes['classes'],
         "boxes": boxes,
     }
      
-    return {"images": image, "bounding_boxes":boxes} 
+    return {"images": image, "bounding_boxes":bounding_boxes} 
 
 def Data_Loader(annotation_file):
     data=pd.read_csv(annotation_file)
@@ -75,12 +73,12 @@ def Data_Loader(annotation_file):
 
     data_groups=data.groupby('filename')
     Dataset={'images':[], 'bounding_boxes':[]}
-    
+  
     ngroups=len(data_groups)
     i=0
     for image_name, groups in data_groups:
         image=load_img(image_name, target_size=(224, 224))
-    
+
         rows=[]
         for _, row in groups.iterrows():
             rows.append(row['bounding_boxes'])
@@ -89,23 +87,31 @@ def Data_Loader(annotation_file):
         boxes_classes=ast.literal_eval(rows[1])
 
         BBox_Data_Per_image={'boxes':boxes_coords, 'classes':boxes_classes}
-        Dataset['images'].append(image)
-        Dataset['bounding_boxes'].append(BBox_Data_Per_image)
+        Dataset['images'].append(tf.data.Dataset.from_tensor_slices(image))
+        Dataset['bounding_boxes'].append(tf.data.Dataset.from_tensor_slices(BBox_Data_Per_image))
 
         i+=1
         Progres_bar(i,ngroups)
 
         if(i/ngroups>0.005):break
 
-    Dataset=tf.data.Dataset.from_tensor_slices(Dataset)
+    Built_DBB=Dataset['bounding_boxes'][0]
+    Built_DBI=Dataset['images'][0]
+  
+    for i in range(1, len(Dataset['bounding_boxes'])):
+        Built_DBB=Built_DBB.concatenate(Dataset['bounding_boxes'][i])
+        Built_DBI=Built_DBI.concatenate(Dataset['images'][i])
     
+    Dataset=tf.data.Dataset.zip((Built_DBI, Built_DBB))
+    #Dataset=tf.data.Dataset.from_tensor_slices((Dataset['images'], Dataset['bounding_boxes']))
+    print('\n')
     return(Dataset)
 
 if __name__=='__main__':
-    Dataset=Data_Loader('../Bounding Box Annotations ex.csv')
-
-    #Dataset=Dataset.map(lambda x: Unpack_Raw_Format(x ,'xyxy'))
-
-    #Dataset=Dataset.ragged_batch(5)
+    Dataset=Data_Loader('../Bounding Box Annotations.csv')
+    print(Dataset)
+    Dataset=Dataset.map(lambda x, y: Unpack_Raw_Format(x,y ,'xyxy'))
+    print(Dataset)
+    Dataset=Dataset.ragged_batch(5)
     #print(Dataset)
-    #visualize_dataset(Dataset, bounding_box_format="xyxy", value_range=(0, 255), rows=2, cols=2)
+    visualize_dataset(Dataset, bounding_box_format="xyxy", value_range=(0, 255), rows=2, cols=2)
